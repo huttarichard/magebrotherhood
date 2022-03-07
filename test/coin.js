c1b9df9a8d5ae976907363f16abcbe165bb7adb9
Richard Hutta <huttarichard@gmail.com>
2022-03-07 15:51:25 +0100

    changes to coin

--- test/coin.js
+++ test/coin.js
@@ -1,10 +1,45 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
 
-describe("Coin", function () {
-  it("should return correct balance", async function () {
+function coinAmount(amount) {
+  return BigNumber.from("1000000000000000000").mul(amount);
+}
+
+describe("Coin contract", function () {
+  let coin, deadline;
+
+  this.beforeEach(async () => {
     const Coin = await ethers.getContractFactory("Coin");
-    const coin = await Coin.deploy();
+    coin = await Coin.deploy(100);
     await coin.deployed();
+    deadline = parseInt(Date.now() / 1000 + 60);
+  });
+
+  it("should mint correct supply at deploy", async function () {
+    await expect(coin.balanceOf(coin.address)).to.be.eq(coinAmount(100));
+    await expect(coin.totalSupply()).to.be.eq(coinAmount(100));
+  });
+
+  it("should swap correcty token for eth", async function () {
+    const [owner] = await ethers.getSigners();
+    const value = ethers.utils.parseEther("100");
+
+    await expect(() => owner.sendTransaction({ to: coin.address, value })).to.changeEtherBalance(coin, value);
+
+    let liqden = await coin.liqudityGuardDenominator();
+    let liqgrd = await coin.liqudityGuard();
+
+    let diff = liqden.sub(liqgrd);
+
+    let price = await coin.getInputPrice(coinAmount(10), coinAmount(90), coinAmount(100));
+    expect(price.div(BigNumber.from("10000000000000000")).toString()).to.be.eq(diff);
+
+    // swap
+    await coin.ethToTokenSwapInput(coinAmount(9), deadline, {
+      value: ethers.utils.parseEther("10"),
+    });
+
+    // todo
   });
 });
