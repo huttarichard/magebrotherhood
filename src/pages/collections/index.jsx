593ac25345d9cc5cac14d68645f94f57d0cfb902
Richard Hutta <huttarichard@gmail.com>
2022-03-18 11:16:16 +0100

    layout fix

--- src/pages/collections/index.jsx
+++ src/pages/collections/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import Layout from "../../components/OldLayout/Layout";
+import Layout from "../../components/Layout/Layout";
 
 export default function CollectionsIndex() {
   return (
