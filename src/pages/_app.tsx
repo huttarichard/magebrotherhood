32ecc6cd2be300c2d35aeacc2cd2eaa1f7efc9bb
Richard Hutta <huttarichard@gmail.com>
2022-03-23 14:04:53 +0100

    useDapp integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import "swiper/css";
 
 import { CacheProvider, EmotionCache } from "@emotion/react";
+import { Config, DAppProvider, Mainnet } from "@usedapp/core";
 import GlobalStyle from "components/ui/GlobalStyle";
 import ThemeProvider, { createEmotionCache } from "components/ui/ThemeProvider";
 import type { AppProps } from "next/app";
@@ -14,6 +15,26 @@ interface Props extends AppProps {
   emotionCache?: EmotionCache;
 }
 
+const config: Config = {
+  autoConnect: true,
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: "https://mainnet.infura.io/v3/5c0736f0b07f4a24a3f0ffc5656b14ad",
+  },
+};
+
+// function Test() {
+//   const { activateBrowserWallet, account } = useEthers();
+//   const etherBalance = useEtherBalance(account);
+//   return (
+//     <div>
+//       {!account && <button onClick={() => activateBrowserWallet()}>Connect</button>}
+//       {account && <p>Account: {account}</p>}
+//       {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+//     </div>
+//   );
+// }
+
 function MageBrotherHoodApp(props: Props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
@@ -24,8 +45,10 @@ function MageBrotherHoodApp(props: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
       </Head>
       <ThemeProvider>
-        <GlobalStyle />
-        <Component {...pageProps} />
+        <DAppProvider config={config}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </DAppProvider>
       </ThemeProvider>
       {/* <Script type="module" src="https://unpkg.com/focus-visible@5.0.2/dist/focus-visible.js" /> */}
     </CacheProvider>
