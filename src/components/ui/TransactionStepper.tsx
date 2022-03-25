9784b8fc77c391eaec06778bfd07ee9c75c2a1cf
Michał Bartczak <probartczak@gmail.com>
2022-03-25 13:30:43 +0100

    transaction stepper

--- src/components/ui/TransactionStepper.tsx
+++ src/components/ui/TransactionStepper.tsx
@@ -1,40 +1,98 @@
+import styled from "@emotion/styled";
 import Box from "@mui/material/Box";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import { StepLabelProps } from "@mui/material/StepLabel";
 import Stepper from "@mui/material/Stepper";
-import * as React from "react";
 
-type Step = {
+type TransactionStepperProps = {
+  activeStep: number;
+  hash?: string;
+};
+
+type IStep = {
+  error: boolean;
   label: string;
   labelOptional?: string;
   labelErrorOptional?: string;
-  error: boolean;
+  content?: string | JSX.Element;
 };
 
-const steps: Step[] = [
+const steps: IStep[] = [
   {
+    error: false,
     label: "Initiating",
     labelOptional: "GO!",
-    error: false,
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis veritatis quia eligendi ipsum ex tempore
+        sapiente ea consequatur quisquam fugiat corrupti minima, aut omnis. Reprehenderit non facilis repellendus
+        praesentium architecto.
+      </p>
+    ),
   },
   {
+    error: false,
     label: "Waiting for confirmation",
     labelOptional: "Wait for it...",
-    error: false,
+    content: (
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati autem quisquam similique magni architecto
+        voluptatibus qui eius sit iure eveniet libero eum, quia, sapiente minima molestias eaque modi ducimus
+        voluptatum?
+      </p>
+    ),
   },
   {
-    label: "Transaction confirmed",
-    labelOptional: "Congrats!",
-    labelErrorOptional: "Failed!",
     error: false,
+    label: "Finalizing",
+    labelOptional: "Almost there!",
+    labelErrorOptional: "Failed!",
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, aut officia et possimus ratione, in provident
+        magnam minus qui hic odit quis enim praesentium numquam deleniti adipisci aperiam optio quasi? Ipsam inventore
+        consequatur accusantium ex? Magni animi, adipisci doloremque temporibus distinctio commodi consequuntur tenetur?
+      </p>
+    ),
   },
 ];
 
-export default function TransactionStepper({ activeStep }: { activeStep: number }) {
+const StepContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    margin: 0 0 2rem;
+    color: #fff;
+    text-align: center;
+    font-size: 3rem;
+    font-family: "Bebas Neue", sans-serif;
+  }
+`;
+
+const StepContentText = styled(Box)`
+  color: #fff;
+  text-align: center;
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const Hash = styled.div`
+  h3 {
+    color: #fff;
+  }
+
+  a {
+    color: ${({ theme }) => theme.palette.primary.main};
+  }
+`;
+
+export default function TransactionStepper({ activeStep, hash }: TransactionStepperProps) {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep}>
+    <Box>
+      <Stepper activeStep={activeStep} sx={{ mb: 4 }}>
         {steps.map((step, index) => {
           const labelProps: StepLabelProps = {
             error: step.error,
@@ -55,6 +113,16 @@ export default function TransactionStepper({ activeStep }: { activeStep: number 
           );
         })}
       </Stepper>
+      <StepContent>
+        <h2>{activeStep === steps.length ? "Transaction complete" : steps.find((_, i) => i === activeStep)?.label}</h2>
+        <StepContentText>{steps.find((_, i) => i === activeStep)?.content}</StepContentText>
+        {hash && (
+          <Hash>
+            <h3>Transaction hash</h3>
+            <a href="#">{hash}</a>
+          </Hash>
+        )}
+      </StepContent>
     </Box>
   );
 }
