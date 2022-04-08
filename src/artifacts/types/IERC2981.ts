195390047fd525ea3959b5adf0f519925b88f490
Petr Brazdil <p.brazdil@gmail.com>
2022-04-08 17:26:30 +0200

    Tests for Coin, Playables, Promoters

--- src/artifacts/types/IERC2981.ts
+++ src/artifacts/types/IERC2981.ts
@@ -13,7 +13,12 @@ import {
 } from "ethers";
 import { FunctionFragment, Result } from "@ethersproject/abi";
 import { Listener, Provider } from "@ethersproject/providers";
-import { TypedEventFilter, TypedEvent, TypedListener, OnEvent } from "./common";
+import type {
+  TypedEventFilter,
+  TypedEvent,
+  TypedListener,
+  OnEvent,
+} from "./common";
 
 export interface IERC2981Interface extends utils.Interface {
   contractName: "IERC2981";
--- test/staking.ts
+++ test/staking.ts
@@ -1,22 +1,21 @@
 /// <reference types="@nomiclabs/hardhat-waffle" />
 
-import { BigNumber } from "@ethersproject/bignumber";
 import { ethers } from "hardhat";
 
 import {
-  Affiliate,
-  Affiliate__factory as AffiliateFactory,
   Coin,
   Coin__factory as CoinFactory,
   Playables,
   Playables__factory as PlayablesFactory,
+  Promoter,
+  Promoter__factory as PromoterFactory,
   Staking,
   Staking__factory as StakingFactory,
 } from "../src/artifacts/types";
 
 describe("Playables contract", function () {
   let coin: Coin;
-  let affiliate: Affiliate;
+  let promoter: Promoter;
   let playables: Playables;
   let staking: Staking;
 
@@ -30,17 +29,17 @@ describe("Playables contract", function () {
     const value = ethers.utils.parseEther("100");
     await owner.sendTransaction({ to: coin.address, value });
 
-    await coin.setLiqudityGuard(BigNumber.from(0), BigNumber.from(1));
-    await coin.setTaxFee(BigNumber.from(0), BigNumber.from(1));
+    // await coin.setLiqudityGuard(BigNumber.from(0), BigNumber.from(1));
+    // await coin.setTaxFee(BigNumber.from(0), BigNumber.from(1));
 
-    const affiliateFactory = (await ethers.getContractFactory("Affiliate", owner)) as AffiliateFactory;
-    affiliate = await affiliateFactory.deploy(coin.address);
+    const promoterFactory = (await ethers.getContractFactory("Promoter", owner)) as PromoterFactory;
+    promoter = await promoterFactory.deploy(coin.address);
 
-    await affiliate.grantRole(await affiliate.REWARDER(), owner.address);
-    await affiliate.allowRewarding(owner.address, 100);
+    await promoter.grantRole(await promoter.MANAGER(), owner.address);
+    // await promoter.allowRewarding(owner.address, 100);
 
     const Playables = (await ethers.getContractFactory("Playables", owner)) as PlayablesFactory;
-    playables = await Playables.deploy(coin.address, affiliate.address, "");
+    playables = await Playables.deploy(coin.address, promoter.address, "");
 
     const Staking = (await ethers.getContractFactory("Staking", owner)) as StakingFactory;
     staking = await Staking.deploy(100, 100, coin.address);
