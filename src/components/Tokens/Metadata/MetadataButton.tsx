6decd9c1ba4b5f716d4ab357a43e6eac3fe7717f
Richard Hutta <huttarichard@gmail.com>
2022-04-19 11:50:14 +0200

    metadata modal

--- src/components/Tokens/Metadata/MetadataButton.tsx
+++ src/components/Tokens/Metadata/MetadataButton.tsx
@@ -1,20 +1,26 @@
 import styled from "@emotion/styled";
 import { faBinary } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Drawer from "@mui/material/Drawer";
 import BadgeButton, { BadgeButtonProps } from "components/ui/BadgeButton";
 import Modal from "components/ui/Modal";
 import { FullToken } from "hooks/useTokens";
 import { useState } from "react";
+import { useWindowSize } from "react-use";
 
 interface ButtonProps extends Omit<BadgeButtonProps, "icon"> {
   token: FullToken;
 }
 
 const Code = styled.div`
+  padding: 10px;
+  max-height: 600px;
+  overflow: scroll;
+
   pre {
     max-width: 600px;
     overflow-y: scroll;
-    font-size: 15px;
+    font-size: 10px;
   }
 
   h1 {
@@ -36,12 +42,41 @@ export default function MetadataButton({ token, ...props }: ButtonProps) {
         View Metadata
       </BadgeButton>
 
-      <Modal open={open} onClose={() => setOpen(false)}>
+      <ModalMetadata open={open} token={token} onClose={() => setOpen(false)}></ModalMetadata>
+    </>
+  );
+}
+
+interface ModalMetadataProps {
+  token: FullToken;
+  open: boolean;
+  onClose?: () => void;
+}
+
+export function ModalMetadata({ token, open, onClose }: ModalMetadataProps) {
+  const { width } = useWindowSize();
+
+  if (width <= 600) {
+    return (
+      <Drawer anchor="bottom" open={open} onClose={onClose}>
         <Code>
           <h1>IPFS Data</h1>
+          <p>This information represent data directly stored on IPFS</p>
+          <hr />
           <pre>{JSON.stringify(token, null, 2)}</pre>
         </Code>
-      </Modal>
-    </>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Modal open={open} onClose={onClose} wsx={{ width: "calc(100% - 30px)", maxWidth: "600px" }}>
+      <Code>
+        <h1>IPFS Data</h1>
+        <p>This information represent data directly stored on IPFS</p>
+        <hr />
+        <pre>{JSON.stringify(token, null, 2)}</pre>
+      </Code>
+    </Modal>
   );
 }
